694aaa533c3732a9f6ac3a73ffc08ae1cd105070
tplinux <2285020853@qq.com>
2016-10-21 20:07:52 +0800

    Create gitconfig.sh

---
+++ gitconfig.sh
@@ -0,0 +1,10 @@
+git config --global user.email '2285020853@qq.cmo'
+git config --global user.name 'hyhlinux'
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+
+git config --global commit.template   /h/github/w7_git_template.txt
+git config --global core.editor vim
